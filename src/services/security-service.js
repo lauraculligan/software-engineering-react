26b0a8224394b01dbafbcd7a2ced8a8064efc1b1
Laura Culligan <lgculligan@gmail.com>
2022-03-26 17:37:42 -0400

    base_url edit

--- src/services/security-service.js
+++ src/services/security-service.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-// const BASE_URL = "https://cs5500-01-sp22.herokuapp.com/api";
-// const BASE_URL = "http://localhost:4000/api";
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "https://fse-tuiter-node-a4.herokuapp.com";
+//const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const SECURITY_API = `${BASE_URL}/api/auth`;
 
