96d4cdec677c4097401be3a0f9e1bf159ae5481b
Laura Culligan <lgculligan@gmail.com>
2022-03-28 21:02:54 -0400

    add on click for thumbs down

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -12,8 +12,6 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
           {tuit.stats && tuit.stats.retuits}
         </div>
         <div className="col">
-          <i className="fas fa-heart me-1"></i>
-            {tuit.stats && tuit.stats.likes}
           <span onClick={() => likeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
@@ -27,7 +25,17 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
           </span>
         </div>
         <div className="col">
-          <i className="far fa-thumbs-up"></i>
+          <span onClick={() => likeTuit(tuit)}>
+              {
+                  tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                  <i className="fas thumbs-down me-1" style={{color: 'black'}}></i>
+              }
+              {
+                  tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  <i className="far thumbs-down me-1" style={{color: 'black'}}></i>
+              }
+              {tuit.stats && tuit.stats.likes}
+          </span>
         </div>
         <div className="col">
           <i className="far fa-inbox-out"></i>
