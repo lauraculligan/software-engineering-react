1b1cde3197d3762c88fe9641e3cfcc3f00f1102c
Laura Culligan <lgculligan@gmail.com>
2022-03-28 20:41:50 -0400

    make heart always appear

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -12,14 +12,15 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
           {tuit.stats && tuit.stats.retuits}
         </div>
         <div className="col">
+            <i className="fas fa-heart me-1"></i>
           <span onClick={() => likeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                  <i className="fas fa-heart" style={{color: 'red'}}></i>
+                  <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
               }
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-                  <i className="far fa-heart" style="color:red"></i>
+                  <i className="far fa-heart me-1" style="color:red"></i>
               }
             {tuit.stats && tuit.stats.likes}
           </span>
