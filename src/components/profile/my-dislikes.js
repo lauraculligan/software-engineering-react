34d1f006b663eab33368c2d09e1ab87637df5a34
Laura Culligan <lgculligan@gmail.com>
2022-03-30 08:21:42 -0400

    add dislikes screen

---
+++ src/components/profile/my-dislikes.js
@@ -0,0 +1,18 @@
+import Tuits from "../tuits";
+import * as service from "../../services/dislikes-service";
+import {useEffect, useState} from "react";
+
+const MyDisLikes = () => {
+    const [dislikedTuits, setDisLikedTuits] = useState([]);
+    const findTuitsIDisLike = () =>
+        service.findAllTuitsDisLikedByUser("me")
+            .then((tuits) => setDisLikedTuits(tuits));
+    useEffect(findTuitsIDisLike, []);
+
+    return(
+        <div>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIDisLike}/>
+        </div>
+    );
+};
+export default MyDisLikes;
